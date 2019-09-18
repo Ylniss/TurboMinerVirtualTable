514e759b946e3a931e9ad173f32e97e6d13593e4
Yolan <zupqa0@gmail.com>
2019-09-18 22:50:59 +0200

    beginning of element counters

---
+++ TurboMinerVirtualTable.Unity/Assets/Scripts/Ui/ConfigUpArrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigUpArrow : MonoBehaviour
+{
+    public void UpCounter()
+    {
+        var elementCounter = GameObject.Find("ElementCounter");
+
+        var currentText = elementCounter.GetComponent<InputField>().text;
+
+        var abc = currentText;
+    }
+
+}
